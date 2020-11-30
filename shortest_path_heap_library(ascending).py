20db2e7561876810c7e7608bf95a0f2aaef08ce8
cksrb1616 <53897355+cksrb1616@users.noreply.github.com>
2020-11-30 20:45:47 +0100

    new

--- shortest_path_heap_library(ascending).py
+++ shortest_path_heap_library(ascending).py
@@ -3,7 +3,7 @@ def heapsort(iterable):
     h = []
     result = []
     for value in iterable:
-        heapq.heapush(h, value)
+        heapq.heappush(h, value)
     for i in range(len(h)):
         result.append(heapq.heappop(h))
     return result
