08afd83151f7bc9e0f65cbecf5f95408767c812e
Minjun <cksrb1616@gmail.com>
2021-03-29 11:03:17 +0200

    Update shortest_path_9-1_Dijkstra(simple).py

--- shortest_path/shortest_path_9-1_Dijkstra(simple).py
+++ shortest_path/shortest_path_9-1_Dijkstra(simple).py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-n,m map(int,input().split())
+n,m = map(int,input().split())
 start = int(input())
 graph = [[] for i in range(n+1)]
 visited = [False] * (n+1)
@@ -17,7 +17,7 @@ def get_smallest_node():
     index = 0
     for i in range(1,n+1):
         if distance[i]< min_value and not visited[i]:
-            min_value = distnace[i]
+            min_value = distance[i]
             index = i
         return index
 def dijkstar(start):
